c61023eeb95e926000aa47b559f5994c235cbd29
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2022-10-07 01:11:15 +0900

    add rust tutorial
    add rust tutorial

---
+++ hello-rust/target/debug/deps/hello_rust-1d7519881d6a0a5e.d
@@ -0,0 +1,5 @@
+/home/chiya/Works/hello_rust/hello-rust/target/debug/deps/hello_rust-1d7519881d6a0a5e: src/main.rs
+
+/home/chiya/Works/hello_rust/hello-rust/target/debug/deps/hello_rust-1d7519881d6a0a5e.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/chiya/Works/hello_rust/hello-rust/target/debug/hello-rust: /home/chiya/Works/hello_rust/hello-rust/src/main.rs
